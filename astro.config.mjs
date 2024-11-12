842b0e1c1d243d14c5f6ce64223450cd5d7469e9
Régis Tremblay Lefrançois <regg00@gmail.com>
2024-11-12 14:49:22 -0500

    upgraded to latest and added localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,13 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://itrel.ca'
+  site: 'https://itrel.ca',
+  output: 'static',
+  i18n: {
+    defaultLocale: "fr",
+    locales: ["en","fr"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
